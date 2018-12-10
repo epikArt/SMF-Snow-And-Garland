0259923ad80162acce1ada50a3d4ca84d29249cc
Bomber Code <contacto@bombercode.net>
2018-12-09 22:12:48 -0300

    Translation to Latin Spanish

---
+++ Themes/default/languages/SnowAndGarland/.spanish_latin-utf8.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package SMF Snow and Garland
+ * @author digger <digger@mysmf.ru> <http://mysmf.ru>
+ * @copyright Copyright (c) 2011-2016, digger
+ * @license BSD License
+ * @version 1.3
+ * Spanish translation https://www.bombercode.net Copyright 2014-2018
+ */
+ 
+$txt['SnowAndGarland'] = 'Nieve y guirnalda';
+$txt['SnowAndGarland_garland_enabled'] = 'Activar guirnalda';
+$txt['SnowAndGarland_garland_mobile_enabled'] = 'Activar garland en dispositivos móviles también';
+$txt['SnowAndGarland_garland_sound_enabled'] = 'Activar sonido de guirnalda';
+$txt['SnowAndGarland_top_offset'] = 'Página superior del foro offset (px)';
+$txt['SnowAndGarland_garland_top_offset'] = 'Guirnalda superior offset (px)';
+$txt['SnowAndGarland_garland_garlandSize'] = 'Tamaño de las lámparas';
+$txt['SnowAndGarland_snow_enabled'] = 'Activar la nieve';
+$txt['SnowAndGarland_snow_mobile_enabled'] = 'Activar la nieve en dispositivos móviles también (no recomendado)';
+$txt['SnowAndGarland_snow_flakesMax'] = 'Cantidad máxima de copos de nieve';
+$txt['SnowAndGarland_snow_flakesMaxActive'] = 'Cantidad máxima de copos de nieve activos';
+$txt['SnowAndGarland_snow_followMouse'] = 'Nieve siga el ratón';
+$txt['SnowAndGarland_snow_snowColor'] = 'Color de la nieve (código hexadecimal)';
+$txt['SnowAndGarland_snow_flakeHeight'] = 'Altura máxima de píxeles reservada para el elemento de nieve (px)';
+$txt['SnowAndGarland_snow_snowCharacter'] = 'Personaje de nieve';
+$txt['SnowAndGarland_snow_useMeltEffect'] = 'Efecto para derretir la nieve';
+$txt['SnowAndGarland_snow_useTwinkleEffect'] = 'Efecto centelleante para la nieve.';
+$txt['SnowAndGarland_snow_snowStick'] = 'Palo de nieve';
+$txt['SnowAndGarland_snow_vMaxX'] = 'Rango máximo de velocidad X para la nieve';
+$txt['SnowAndGarland_snow_vMaxY'] = 'Rango máximo de velocidad Y para nieve';
+$txt['SnowAndGarland_snow_zIndex'] = 'Z-Index (Orden de apilamiento CSS aplicado a cada copo de nieve.)';
+$txt['SnowAndGarland_snow_targetElement'] = 'El elemento al que se agregará la nieve puede ser un ID de elemento, por ejemplo. myDiv, o una referencia de nodo DOM como document.body';
+?>
